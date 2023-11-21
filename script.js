5bfb02f4d1ec0d5c70fa25459de58aa3c6742e73
startingfrom0rating <yann2009thecool@gmail.com>
2023-11-20 20:05:37 -0500

    a4
    a4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const text = "Y.Y.A.C.C";
 let index = 0;
-let speed = 400; // Speed of typing animation in milliseconds
+let speed = 100; // Speed of typing animation in milliseconds
 
 function typeWriter() {
   if (index < text.length) {
@@ -10,6 +10,23 @@ function typeWriter() {
   } else {
     document.getElementById("entryAnimation").style.display = "none";
     document.querySelector(".password-box").style.display = "block";
+
+    // Display the GIF after the typing animation completes
+    const gif = document.createElement("img");
+    gif.src = "ezgif-4-378bee2568.gif"; // Replace with the local path of your GIF
+    gif.style.width = "200px"; // Adjust width if needed
+    gif.style.height = "200px"; // Adjust height if needed
+    document.body.appendChild(gif);
+
+    // Redirect after a delay following the GIF display
+    setTimeout(function() {
+      const enteredPassword = document.getElementById("passwordInput").value;
+      if (enteredPassword === "Origins") {
+        window.location.href = "https://docs.google.com/document/d/1CvLDK8cSuxz-Gv8-FjUjNHgIzv5Hhyuqz4PE1R7MqNk/edit";
+      } else {
+        document.getElementById("errorMessage").textContent = "Incorrect, contact website owner.";
+      }
+    }, 5000); // Redirect after 5 seconds (adjust as needed)
   }
 }
 
@@ -17,16 +34,3 @@ function typeWriter() {
 window.onload = function() {
   typeWriter();
 };
-
-function checkPassword() {
-  const enteredPassword = document.getElementById("passwordInput").value;
-
-  if (enteredPassword === "Origins") {
-    // Redirect to another site after successful password entry
-    window.location.href = "https://your-redirect-site.com";
-    // You may load the gif here, but due to security restrictions,
-    // loading from GitHub directly may not work without server-side access
-  } else {
-    document.getElementById("errorMessage").textContent = "Incorrect, contact website owner.";
-  }
-}
