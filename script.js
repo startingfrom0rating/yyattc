2c7d25eea3ede613949248053ed4a2e0ea5b43ec
startingfrom0rating <yann2009thecool@gmail.com>
2023-11-20 21:37:46 -0500

    a66
    a66

--- script.js
+++ script.js
@@ -11,22 +11,51 @@ function typeWriter() {
     document.getElementById("entryAnimation").style.display = "none";
     document.querySelector(".password-box").style.display = "block";
 
-    // Display the GIF after the typing animation completes
-    const gif = document.createElement("img");
-    gif.src = "ezgif-4-378bee2568.gif"; // Replace with the local path of your GIF
-    gif.style.width = "200px"; // Adjust width if needed
-    gif.style.height = "200px"; // Adjust height if needed
-    document.body.appendChild(gif);
+    function playGif() {
+      const gif = document.createElement("img");
+      gif.src = "ezgif-4-378bee2568.gif"; // Replace with the local path of your GIF
+      gif.style.width = "200px"; // Adjust width if needed
+      gif.style.height = "200px"; // Adjust height if needed
+      document.body.appendChild(gif);
 
-    // Redirect after a delay following the GIF display
-    setTimeout(function() {
-      const enteredPassword = document.getElementById("passwordInput").value;
-      if (enteredPassword === "Origins") {
-        window.location.href = "https://docs.google.com/document/d/1CvLDK8cSuxz-Gv8-FjUjNHgIzv5Hhyuqz4PE1R7MqNk/edit";
-      } else {
-        document.getElementById("errorMessage").textContent = "Incorrect, contact website owner.";
+      const grantedMessage = document.createElement("span");
+      grantedMessage.textContent = "Access Granted.";
+      grantedMessage.style.color = "#fff";
+      grantedMessage.style.fontFamily = "Times New Roman, Times, serif";
+      grantedMessage.style.fontSize = "24px";
+      grantedMessage.style.animation = "flash 0.5s infinite";
+      document.body.appendChild(grantedMessage);
+
+      const redirectMessage = document.createElement("span");
+      redirectMessage.textContent = "Redirecting to document...";
+      redirectMessage.style.color = "#fff";
+      redirectMessage.style.fontFamily = "Times New Roman, Times, serif";
+      redirectMessage.style.fontSize = "24px";
+      redirectMessage.style.animation = "flash 0.5s infinite";
+      redirectMessage.style.marginTop = "10px";
+      document.body.appendChild(redirectMessage);
+
+      setTimeout(function() {
+        grantedMessage.remove();
+        redirectMessage.remove();
+        gif.remove();
+
+        const enteredPassword = document.getElementById("passwordInput").value;
+        if (enteredPassword === "Origins") {
+          window.location.href = "https://docs.google.com/document/d/1CvLDK8cSuxz-Gv8-FjUjNHgIzv5Hhyuqz4PE1R7MqNk/edit";
+        } else {
+          document.getElementById("errorMessage").textContent = "Incorrect, contact website owner.";
+        }
+      }, 5000); // Redirect after 5 seconds (adjust as needed)
+    }
+
+    document.getElementById("passwordInput").addEventListener("keyup", function(event) {
+      if (event.key === "Enter") {
+        playGif();
       }
-    }, 5000); // Redirect after 5 seconds (adjust as needed)
+    });
+
+    document.querySelector("button").onclick = playGif;
   }
 }
 
