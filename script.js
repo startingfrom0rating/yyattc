22cef6e63fba25e1f39d7a3f7c3149f778323a15
startingfrom0rating <yann2009thecool@gmail.com>
2023-11-20 22:00:05 -0500

    a9
    9

--- script.js
+++ script.js
@@ -11,58 +11,51 @@ function typeWriter() {
     document.getElementById("entryAnimation").style.display = "none";
     document.querySelector(".password-box").style.display = "block";
 
-    function handleCorrectPassword() {
-      const enteredPassword = document.getElementById("passwordInput").value;
-      if (enteredPassword === "Origins") {
-        document.getElementById("passwordInput").style.display = "none";
+    function playGif() {
+      const gif = document.createElement("img");
+      gif.src = "ezgif-4-378bee2568.gif"; // Replace with the local path of your GIF
+      gif.style.width = "200px"; // Adjust width if needed
+      gif.style.height = "200px"; // Adjust height if needed
+      document.body.appendChild(gif);
 
-        const grantedMessage = document.createElement("span");
-        grantedMessage.textContent = "Access Granted.";
-        grantedMessage.style.color = "green";
-        grantedMessage.style.fontFamily = "Times New Roman, Times, serif";
-        grantedMessage.style.fontSize = "24px";
-        grantedMessage.style.animation = "flash-green 0.5s infinite";
-        document.body.appendChild(grantedMessage);
+      const grantedMessage = document.createElement("span");
+      grantedMessage.textContent = "Access Granted.";
+      grantedMessage.style.color = "#fff";
+      grantedMessage.style.fontFamily = "Times New Roman, Times, serif";
+      grantedMessage.style.fontSize = "24px";
+      grantedMessage.style.animation = "flash 0.5s infinite";
+      document.body.appendChild(grantedMessage);
 
-        const redirectMessage = document.createElement("span");
-        redirectMessage.textContent = "Redirecting to document...";
-        redirectMessage.style.color = "green";
-        redirectMessage.style.fontFamily = "Times New Roman, Times, serif";
-        redirectMessage.style.fontSize = "24px";
-        redirectMessage.style.animation = "flash-green 0.5s infinite";
-        redirectMessage.style.marginTop = "10px";
-        document.body.appendChild(redirectMessage);
+      const redirectMessage = document.createElement("span");
+      redirectMessage.textContent = "Redirecting to document...";
+      redirectMessage.style.color = "#fff";
+      redirectMessage.style.fontFamily = "Times New Roman, Times, serif";
+      redirectMessage.style.fontSize = "24px";
+      redirectMessage.style.animation = "flash 0.5s infinite";
+      redirectMessage.style.marginTop = "10px";
+      document.body.appendChild(redirectMessage);
 
-        setTimeout(function() {
-          grantedMessage.remove();
-          redirectMessage.remove();
+      setTimeout(function() {
+        grantedMessage.remove();
+        redirectMessage.remove();
+        gif.remove();
 
-          const gif = document.createElement("img");
-          gif.src = "ezgif-4-378bee2568.gif"; // Replace with the local path of your GIF
-          gif.style.width = "200px"; // Adjust width if needed
-          gif.style.height = "200px"; // Adjust height if needed
-          gif.style.position = "absolute";
-          gif.style.top = "50%";
-          gif.style.left = "50%";
-          gif.style.transform = "translate(-50%, -50%)";
-          document.body.appendChild(gif);
-
-          setTimeout(function() {
-            window.location.href = "https://docs.google.com/document/d/1CvLDK8cSuxz-Gv8-FjUjNHgIzv5Hhyuqz4PE1R7MqNk/edit";
-          }, 5000); // Redirect after 5 seconds (adjust as needed)
-        }, 500); // Wait 0.5 seconds before showing GIF and redirect messages
-      } else {
-        document.getElementById("errorMessage").textContent = "Incorrect, contact website owner.";
-      }
+        const enteredPassword = document.getElementById("passwordInput").value;
+        if (enteredPassword === "Origins") {
+          window.location.href = "https://docs.google.com/document/d/1CvLDK8cSuxz-Gv8-FjUjNHgIzv5Hhyuqz4PE1R7MqNk/edit";
+        } else {
+          document.getElementById("errorMessage").textContent = "Incorrect, contact website owner.";
+        }
+      }, 5000); // Redirect after 5 seconds (adjust as needed)
     }
 
     document.getElementById("passwordInput").addEventListener("keyup", function(event) {
       if (event.key === "Enter") {
-        handleCorrectPassword();
+        playGif();
       }
     });
 
-    document.querySelector("button").onclick = handleCorrectPassword;
+    document.querySelector("button").onclick = playGif;
   }
 }
 
