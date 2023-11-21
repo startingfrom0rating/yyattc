847615dff04f3bf78214c4efbb5f9950701dc3b9
startingfrom0rating <yann2009thecool@gmail.com>
2023-11-20 22:16:59 -0500

    a7
    a7

--- script.js
+++ script.js
@@ -11,22 +11,32 @@ function typeWriter() {
     document.getElementById("entryAnimation").style.display = "none";
     document.querySelector(".password-box").style.display = "block";
 
-    // Display the GIF after the typing animation completes
-    const gif = document.createElement("img");
-    gif.src = "ezgif-4-378bee2568.gif"; // Replace with the local path of your GIF
-    gif.style.width = "200px"; // Adjust width if needed
-    gif.style.height = "200px"; // Adjust height if needed
-    document.body.appendChild(gif);
-
-    // Redirect after a delay following the GIF display
-    setTimeout(function() {
+    // Redirect after confirming the password
+    function redirectAfterPasswordConfirmation() {
       const enteredPassword = document.getElementById("passwordInput").value;
       if (enteredPassword === "Origins") {
-        window.location.href = "https://docs.google.com/document/d/1CvLDK8cSuxz-Gv8-FjUjNHgIzv5Hhyuqz4PE1R7MqNk/edit";
+        const gif = document.createElement("img");
+        gif.src = "ezgif-4-378bee2568.gif"; // Replace with the local path of your GIF
+        gif.style.width = "200px"; // Adjust width if needed
+        gif.style.height = "200px"; // Adjust height if needed
+        document.body.appendChild(gif);
+
+        setTimeout(function() {
+          window.location.href = "https://docs.google.com/document/d/1CvLDK8cSuxz-Gv8-FjUjNHgIzv5Hhyuqz4PE1R7MqNk/edit";
+        }, 5000); // Redirect after 5 seconds (adjust as needed)
       } else {
         document.getElementById("errorMessage").textContent = "Incorrect, contact website owner.";
       }
-    }, 5000); // Redirect after 5 seconds (adjust as needed)
+    }
+
+    // Redirect after password check when Enter button is clicked
+    document.getElementById("passwordInput").addEventListener("keyup", function(event) {
+      if (event.key === "Enter") {
+        redirectAfterPasswordConfirmation();
+      }
+    });
+
+    document.querySelector("button").onclick = redirectAfterPasswordConfirmation;
   }
 }
 
