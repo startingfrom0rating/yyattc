3d94566d5cf076d597f922b344f120a383562b80
startingfrom0rating <128187615+startingfrom0rating@users.noreply.github.com>
2023-11-28 19:47:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const text = "Y.Y.A.C.C";
 let index = 0;
-let speed = 500; // Speed of typing animation in milliseconds
+let speed = 700; // Speed of typing animation in milliseconds
 
 function typeWriter() {
   if (index < text.length) {
@@ -11,32 +11,22 @@ function typeWriter() {
     document.getElementById("entryAnimation").style.display = "none";
     document.querySelector(".password-box").style.display = "block";
 
-    // Redirect after confirming the password
-    function redirectAfterPasswordConfirmation() {
+    // Display the GIF after the typing animation completes
+    const gif = document.createElement("img");
+    gif.src = "ezgif-4-378bee2568.gif"; // Replace with the local path of your GIF
+    gif.style.width = "200px"; // Adjust width if needed
+    gif.style.height = "200px"; // Adjust height if needed
+    document.body.appendChild(gif);
+
+    // Redirect after a delay following the GIF display
+    setTimeout(function() {
       const enteredPassword = document.getElementById("passwordInput").value;
       if (enteredPassword === "Origins") {
-        const gif = document.createElement("img");
-        gif.src = "ezgif-4-378bee2568.gif"; // Replace with the local path of your GIF
-        gif.style.width = "200px"; // Adjust width if needed
-        gif.style.height = "200px"; // Adjust height if needed
-        document.body.appendChild(gif);
-
-        setTimeout(function() {
-          window.location.href = "bit.ly/3N0VJL9";
-        }, 2000); // Redirect after 5 seconds (adjust as needed)
+        window.location.href = "https://bit.ly/3N0VJL9"; // Redirect to the specified URL
       } else {
         document.getElementById("errorMessage").textContent = "Incorrect, contact website owner.";
       }
-    }
-
-    // Redirect after password check when Enter button is clicked
-    document.getElementById("passwordInput").addEventListener("keyup", function(event) {
-      if (event.key === "Enter") {
-        redirectAfterPasswordConfirmation();
-      }
-    });
-
-    document.querySelector("button").onclick = redirectAfterPasswordConfirmation;
+    }, 5000); // Redirect after 5 seconds (adjust as needed)
   }
 }
 
@@ -44,4 +34,3 @@ function typeWriter() {
 window.onload = function() {
   typeWriter();
 };
-// this is a message
